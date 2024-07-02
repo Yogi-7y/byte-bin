8dc598141ce2c7a334f7b2309964e3e77f2f17cb
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-07-02 21:10:45 +0530

    Add routes for viewing and creating bytes

--- main.go
+++ main.go
@@ -6,12 +6,27 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Welcome to Byte bin!"))
+}
+
+func byteView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Viewing a specific byte!"))
+}
+
+func byteCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating a new byte!"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/byte/view", byteView)
+	mux.HandleFunc("/byte/create", byteCreate)
 
 	log.Print("Server started on localhost:4000")
 	err := http.ListenAndServe(":4000", mux)
