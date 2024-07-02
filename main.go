f0d234ed004770f83abaafbfacc84434d31fae04
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-07-02 21:00:15 +0530

    Add HTTP server and handle root route

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to Byte bin!"))
+}
 
 func main() {
-	fmt.Println("Welcome to Byte bin!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Server started on localhost:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
